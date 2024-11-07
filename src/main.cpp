904c93423bb51b5b90eccb67a0751859f7d84a0b
chomp105 <outdoor10life@gmail.com>
2024-11-06 20:45:03 -0600

    fake collision check

--- src/main.cpp
+++ src/main.cpp
@@ -21,68 +21,22 @@ void setRectangle(SDL_Rect& rect, int x, int y, int w, int h) {
     rect = {x, y, w, h};
 };
 
-void drawCircle(SDL_Renderer* renderer, int x_pos, int y_pos) {
-    double r = 20;
-    double t1 = r / 16;
-    int x = x_pos;
-    int y = 0;
-
-    while (x > y) {
-        SDL_Rect rect;
-
-        setRectangle(rect, -x + x_pos, y + y_pos, x * 2, 1);
-        SDL_RenderDrawRect(renderer, &rect);
-        setRectangle(rect, -y + x_pos, x + y_pos - 1, y * 2, 1);
-        SDL_RenderDrawRect(renderer, &rect);
-        setRectangle(rect, -x + x_pos, -y + y_pos, x * 2, 1);
-        SDL_RenderDrawRect(renderer, &rect);
-        setRectangle(rect, -y + x_pos, -x + y_pos + 1, y * 2, 1);
-        SDL_RenderDrawRect(renderer, &rect);
-  
-        y++;
-        t1 += y;
-        double t2 = t1 - x;
-        if (t2 >= 0) {
-            t1 = t2;
-            x--;
-        }
-    }
-}
-
 int main() {
 
     // Data Abstraction:
 
+    Renderer renderer;
     int quit = 0;
 
     // Input:
-    
+
     // Process:
 
-    SDL_Window*   window   = NULL;
-	SDL_Renderer* renderer = NULL;
-
-	//Initialize SDL
-	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-		printf( "init error - SDL_Error: %s\n", SDL_GetError() );
-		return 1;
-	}
-
-	//Create window
-	window = SDL_CreateWindow("ENGINE",
-	SDL_WINDOWPOS_UNDEFINED,
-	SDL_WINDOWPOS_UNDEFINED,
-	SCREEN_WIDTH, SCREEN_HEIGHT,
-	SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
-
-    if (window == NULL) {
-		cout << "window error - SDL_Error: " << SDL_GetError() << endl;
-		return 1;
-	}
-
-
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+    //Initialize SDL
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        printf( "init error - SDL_Error: %s\n", SDL_GetError() );
+        return 1;
+    }
 
     Circle c1 = {(Point){100, 100}, 100, (Color){100, 255, 0}};
     Circle c2 = {(Point){300, 300}, 100, (Color){100, 255, 0}};
@@ -99,6 +53,14 @@ int main() {
         drawCircle(renderer, c1.pos.x, c1.pos.y);
 
         SDL_RenderPresent(renderer);
+
+        for (auto& i : balls) {
+            for (auto& j : monsters) {
+                if (colliding(i, j)) {
+                    
+                }
+            }
+        }
 
         //Update the surface
         SDL_UpdateWindowSurface(window);
