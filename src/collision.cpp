18798e079206c8e2652a95b8c78b3a50aad57d9f
chomp 105 <outdoor10life@gmail.com>
2024-11-28 14:00:31 -0600

    added circle line collision detection

--- src/collision.cpp
+++ src/collision.cpp
@@ -1,15 +1,19 @@
 #include "collision.h"
 
-double getDistance(int xDist, int yDist) {
-    return sqrt(xDist * xDist + yDist * yDist);
+bool areColliding(Circle& i, point& j) {
+    int xDist = j.x - i.p.x;
+    int yDist = j.y - i.p.y;
+    double distance = getDistance(xDist, yDist);
+    double overlap  = i.r - distance;
+    return overlap >= 0;
 }
 
-double getDistance(const point& p1, const point& p2) {
-    return getDistance(p1.x - p2.x, p1.y - p2.y);
-}
-
-bool areColliding(const Circle& c1, const Circle& c2) {
-    return 1;
+bool areColliding(Circle& i, Circle& j) {
+    int xDist = j.p.x - i.p.x;
+    int yDist = j.p.y - i.p.y;
+    double distance = getDistance(xDist, yDist);
+    double overlap  = i.r + j.r - distance;
+    return overlap >= 0;
 }
 
 void checkCollisions(vector<Collision>& collisions, vector<Circle>& group1, vector<Circle>& group2) {
@@ -20,10 +24,36 @@ void checkCollisions(vector<Collision>& collisions, vector<Circle>& group1, vect
                 int yDist = j.p.y - i.p.y;
                 double distance = getDistance(xDist, yDist);
                 double overlap  = i.r + j.r - distance;
-                if (overlap >= 0)
+                if (areColliding(i, j))
                     collisions.push_back((Collision){i, j, xDist, yDist, distance, overlap});
             }
         }
+}
+
+void checkCollisions(vector<LineCollision>& collisions, vector<Circle>& circles, vector<Line>& lines) {
+    for (Circle& c : circles) {
+        for (Line& l : lines) {
+            double ax = c.p.x  - l.p1.x,
+                   bx = l.p2.x - l.p1.x,
+                   ay = c.p.y  - l.p1.y,
+                   by = l.p2.y - l.p1.y;
+            double dot = ax * bx + ay * by;
+            double scalar = dot / l.len();
+
+            double minX = min(l.p1.x, l.p2.x);
+            double maxX = max(l.p1.x, l.p2.x);
+            double minY = min(l.p1.y, l.p2.y);
+            double maxY = max(l.p1.y, l.p2.y);
+
+            point proj = {
+                clamp(scalar * (bx / l.len()) + l.p1.x, minX, maxX),
+                clamp(scalar * (by / l.len()) + l.p1.y, minY, maxY)
+            };
+
+            if (areColliding(c, proj))
+                collisions.push_back({c, proj});
+        }
+    }
 }
 
 void handleCollisions(vector<Collision>& collisions) {
