0ad7b7a1a46009c259b189c41419ecac84e35fd8
chomp 105 <outdoor10life@gmail.com>
2024-11-28 17:59:07 -0600

    added force classes (AGAIN)

---
+++ src/force.h
@@ -0,0 +1,28 @@
+#ifndef force_h
+#define force_h
+
+class force{
+	private:
+		double magnitude;
+		double direction;  // radian
+	
+	public:
+		force();
+		force(double m, double d);
+	
+		void setMagnitude(double m);
+		void setDirection(double d);
+		double getMagnitude()const;
+		double getDirection()const;
+	
+		force operator+(const force& other) const;
+		force operator=(const force& other);
+		force add(const force& other) const;
+		void apply(const force& other);
+	
+		void display(ostream& os)const{
+			os << getMagnitude() << " " << getDirection() << endl;
+		}
+};
+
+#endif // force_h
