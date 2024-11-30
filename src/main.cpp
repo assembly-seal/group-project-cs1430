541b787e51555561f319c6977b7d325e64b93276
chomp 105 <outdoor10life@gmail.com>
2024-11-30 14:35:12 -0600

    remove stupid

--- src/main.cpp
+++ src/main.cpp
@@ -41,18 +41,6 @@ enum RunEvent {
 	MANAGE_ENEMIES
 };
 
-
-// TODO: REMOVE
-void drawCircle(point loc, int size, color c, SDL_Plotter& g){
-	for(double i = -size; i <= size;i+=0.1){
-		for(double j = -size; j <= size; j+=0.1){
-			if(i*i + j*j <= size*size){
-				g.plotPixel(round(loc.x+i),round(loc.y+j),c);
-			}
-		}
-	}
-}
-
 point getUniqueRandomPoint(vector<Circle>& circles) {
 	point p1;
 	bool unique = false;
